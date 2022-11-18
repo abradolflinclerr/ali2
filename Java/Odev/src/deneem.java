d779d650f9499c1e8e9a056a2a4ff0ac76fe2acd
yiğitcan <cakmak02273@gmail.com>
2022-11-18 16:23:43 +0300

    kanka değişti

--- Java/Odev/src/deneem.java
+++ Java/Odev/src/deneem.java
@@ -1,5 +1,45 @@
 public class deneem {
     public static void main(String[] args) {
         System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
     }
 }
