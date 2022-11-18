5766cbec83d91a984b4571fda90d451d1a275810
abradolflinclerr <malikaradag2002@gmail.com>
2022-11-18 16:16:33 +0300

    Aasa

--- Java/Odev/src/deneem.java
+++ Java/Odev/src/deneem.java
@@ -1,5 +1,21 @@
 public class deneem {
     public static void main(String[] args) {
         System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
+        System.out.println("fgnlakdfjgvlfnzbdgbgozp");
     }
 }
